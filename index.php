c7bbc50d34dfae4b15d7fee7920cb1109112cb72
isabu77 <isabu77@gmail.com>
2019-04-05 14:51:33 +0200

    Exercice 1 : si age >= 18 majeur sinon mineur -- terminé

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 //LES CONDITIONS
 
-// exercice 1
-
+// exercice 1 : 
+echo "-- Exercice 1 : si age >= 18 majeur sinon mineur" . "<br>";
+$age = 56;
+if ($age >= 18){
+	echo "Vous êtes majeur" . "<br>";
+}
+else{
+	echo "Vous êtes mineur" . "<br>";
+	
+}
 
 
 ?>
