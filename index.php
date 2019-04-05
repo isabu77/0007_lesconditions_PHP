81936da4e3c11eff15a02d2ab9ea50d3f981195c
isabu77 <isabu77@gmail.com>
2019-04-05 15:21:11 +0200

    Exercice 3 : Genre et age  -- terminé

--- index.php
+++ index.php
@@ -30,7 +30,58 @@ else{
 
 }
 
+// exercice 3 : 
+echo "-- Exercice 3 : " . "<br>";
+$age = 56;
+$genre = "Homme";
+if ($genre == "Homme"){
+	echo "Vous êtes un " . $genre; 
+}else{
+	echo "Vous êtes une " . $genre; 
 
+}
+if ($age >= 18){
+	echo " et vous êtes majeur <br>";
+}else{
+	echo " et vous êtes mineur <br>";
+}
+$age = 15;
+if ($genre == "Homme"){
+	echo "Vous êtes un " . $genre; 
+}else{
+	echo "Vous êtes une " . $genre; 
+
+}
+if ($age >= 18){
+	echo " et vous êtes majeur <br>";
+}else{
+	echo " et vous êtes mineur <br>";
+}
+$age = 56;
+$genre = "Femme";
+if ($genre == "Homme"){
+	echo "Vous êtes un " . $genre; 
+}else{
+	echo "Vous êtes une " . $genre; 
+
+}
+if ($age >= 18){
+	echo " et vous êtes majeur <br>";
+}else{
+	echo " et vous êtes mineur <br>";
+}
+$age = 17;
+if ($genre == "Homme"){
+	echo "Vous êtes un " . $genre; 
+}else{
+	echo "Vous êtes une " . $genre; 
+
+}
+if ($age >= 18){
+	echo " et vous êtes majeur <br>";
+}else{
+	echo " et vous êtes mineur <br>";
+}
 
 ?>
 <!DOCTYPE html>
